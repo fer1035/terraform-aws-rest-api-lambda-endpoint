8e608c41500e0b5443c00af19747519c6c259e20
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-04-07 15:04:08 +0800

    Updated CORS settings.

--- api_endpoint.tf
+++ api_endpoint.tf
@@ -21,9 +21,12 @@ resource "aws_api_gateway_model" "model" {
 resource "aws_api_gateway_method" "method" {
   rest_api_id      = var.api_id
   resource_id      = aws_api_gateway_resource.resource.id
-  http_method      = "POST"
+  http_method      = var.http_method
   authorization    = "NONE"
   api_key_required = true
+  depends_on = [
+    aws_api_gateway_resource.resource
+  ]
   request_models = {
     "application/json" = aws_api_gateway_model.model.name
   }
@@ -42,6 +45,9 @@ resource "aws_api_gateway_integration" "integration" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.function.invoke_arn
+  depends_on = [
+    aws_api_gateway_method.method
+  ]
 }
 
 resource "aws_api_gateway_method_response" "response_200" {
@@ -49,6 +55,9 @@ resource "aws_api_gateway_method_response" "response_200" {
   resource_id = aws_api_gateway_resource.resource.id
   http_method = aws_api_gateway_method.method.http_method
   status_code = "200"
+  depends_on = [
+    aws_api_gateway_method.method
+  ]
 }
 
 resource "aws_lambda_permission" "apigw_lambda" {
@@ -68,12 +77,18 @@ resource "aws_api_gateway_method" "options" {
   http_method      = "OPTIONS"
   authorization    = "NONE"
   api_key_required = false
+  depends_on = [
+    aws_api_gateway_resource.resource
+  ]
 }
 resource "aws_api_gateway_method_response" "options" {
   rest_api_id = var.api_id
   resource_id = aws_api_gateway_resource.resource.id
   http_method = aws_api_gateway_method.options.http_method
   status_code = "200"
+  depends_on = [
+    aws_api_gateway_method.options
+  ]
   response_models = {
     "application/json" = "Empty"
   }
@@ -89,6 +104,9 @@ resource "aws_api_gateway_integration" "options" {
   http_method          = "OPTIONS"
   type                 = "MOCK"
   passthrough_behavior = "WHEN_NO_MATCH"
+  depends_on = [
+    aws_api_gateway_resource.resource
+  ]
   #   # Transforms the incoming XML request to JSON
   #   request_templates = {
   #     "application/xml" = <<EOF
@@ -106,10 +124,13 @@ resource "aws_api_gateway_integration_response" "options" {
   resource_id = aws_api_gateway_resource.resource.id
   http_method = aws_api_gateway_integration.options.http_method
   status_code = "200"
+  depends_on = [
+    aws_api_gateway_integration.options
+  ]
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'${var.http_method},OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'${var.cors}'"
   }
   #   # Transforms the backend JSON response to XML
   #   response_templates = {
